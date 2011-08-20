f2eaccbf64ddbd5d56307bcdad259ee58a98119e
JongMan Koo <jongman@gmail.com>
2011-08-20 06:05:00 -0500

    아카이브 추가

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,8 @@
 from fabric.api import local
 
-def dev():
-    local("rm -rf output/*")
-    local("pelican contents -s settings.py -t theme")
-    local("rm -rf output/theme")
-    local("ln -sf theme/static output/theme")
-
 def build():
     local("pelican contents -s settings.py -t theme")
 
-def build2():
-    local("pelican contents -s settings.py -t notmyidea")
+def watch():
+    local("pelican contents -s settings.py -t theme -r")
+

76cd150034fdaab929b2f43420296741f2da79d5
JongMan Koo <jongman@gmail.com>
2011-08-20 04:49:06 -0500

    I just want one of the simplest themes around

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,11 @@
 from fabric.api import local
 
+def dev():
+    local("rm -rf output/*")
+    local("pelican contents -s settings.py -t theme")
+    local("rm -rf output/theme")
+    local("ln -sf theme/static output/theme")
+
 def build():
     local("pelican contents -s settings.py -t theme")
 
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@
 AUTHOR = u'JongMan'
 SITENAME = u"The Yearly Prophet"
 SITEURL = 'http://blog.andromeda-express.com'
+LOCALE = "ko_KR.utf8"
 
 DISQUS_SITENAME = "theyearlyprophet"
 PDF_GENERATOR = False

85fe34e5fd699ca1a4f6014ee99b7abb6203da5d
JongMan Koo <jongman@gmail.com>
2011-08-20 05:34:26 -0500

    maybe deploy?

---
+++ contents/pages/about.md
@@ -0,0 +1,4 @@
+Slug: about
+Title: About 
+
+안녕하세여 JM 입니당.
--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ LOCALE = "ko_KR.utf8"
 DISQUS_SITENAME = "theyearlyprophet"
 PDF_GENERATOR = False
 DEFAULT_PAGINATION = 5
+WITH_PAGINATION = True
 
 FEED_RSS = 'feeds/rss.xml'
 FEED_MAX_ITEMS = 20
@@ -15,7 +16,7 @@ FEED_MAX_ITEMS = 20
 #DEFAULT_METADATA = (('yeah', 'it is'),)
 
 # static paths will be copied under the same name
-STATIC_PATHS = ["media",]
+STATIC_PATHS = ["images"]
 
 # A list of files to copy from the source to the destination
 FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
